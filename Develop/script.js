e49af2fa86d99f84a0e560fd6539e90835194ef7
Michael <michaelpspore@gmail.com>
2020-10-26 14:06:40 -0500

    Variables and Loop
    setup variable and initial loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,13 @@
 // Assignment code here
 document.querySelector("#generate").addEventListener("click", writePassword);
+// Get references to the #generate element
+
 // Character id arrays
 var charactersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var charactersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charactersSpec = ["!", ")", "@", "(", "#", "*", "$", "&", "%", "^", "+", "/", "-", "?", ">", "<", ":", "{", "}"];
 var charactersNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz'
-
-const uppercaseCharacters = CHARACTERS.split().map((character) => character.toUppercase())
 
 // Variable confirmation
 var confirmCharLength = "";
@@ -17,9 +16,17 @@ var confirmCharSpec;
 var confirmCharLow;
 var confirmCharUpp;
 
+function generatePassword () {
+  var confirmCharLength = (prompt("How many characters would you like for your password?"));
+ 
+//Loop
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+while(confirmCharLength <= 7 || confirmCharLength >= 129) {
+  alert("Password length has to be 8-128 characters long. Please select again.");
+  var confirmCharLength = (prompt("How maany character would you like your password?"));
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -28,8 +35,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
-
+};
+//var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+//generateBtn.addEventListener("click", writePassword)};
