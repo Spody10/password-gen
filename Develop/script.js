bb3318fd0901db8100a714cabb7b662c5deaaf2e
Michael <michaelpspore@gmail.com>
2020-10-26 11:08:29 -0500

    Initial Setup
    initial setup and variables added

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,17 @@
 // Assignment code here
+document.querySelector("#generate").addEventListener("click", writePassword);
+// Character id arrays
+var charactersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var charactersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charactersSpec = ["!", ")", "@", "(", "#", "*", "$", "&", "%", "^", "+", "/", "-", "?", ">", "<", ":", "{", "}"];
+var charactersNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+// Variable confirmation
+var confirmCharLength = "";
+var confirmCharNumb;
+var confirmCharSpec;
+var confirmCharLow;
+var confirmCharUpp;
 
 
 // Get references to the #generate element
@@ -15,3 +28,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
