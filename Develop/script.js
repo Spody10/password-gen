d7affe8f6c04bc67ca6515fa3a37750a87d57f4e
Michael <michaelpspore@gmail.com>
2020-10-27 12:35:14 -0500

    Completed Challenge
    Needs improvement but works properly

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
 // Assignment code here
+//! define a function before you use it
 document.querySelector("#generate").addEventListener("click", writePassword);
 // Get references to the #generate element
 
 // Character id arrays
+//! strings are already arrays.
 var charactersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var charactersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charactersSpec = ["!", ")", "@", "(", "#", "*", "$", "&", "%", "^", "+", "/", "-", "?", ">", "<", ":", "{", "}"];
@@ -10,21 +12,31 @@ var charactersNumb = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
 // Variable confirmation
+//! always set default values.
 var confirmCharLength = "";
 var confirmCharNumb;
 var confirmCharSpec;
 var confirmCharLow;
 var confirmCharUpp;
 
+
+
 function generatePassword () {
   var confirmCharLength = (prompt("How many characters would you like for your password?"));
  
 //Loop confirming password length meets criteria of 8-128 characters
 
+//! Abstract validation into seperate functions.
 while(confirmCharLength <= 7 || confirmCharLength >= 129) {
   alert("Password length has to be 8-128 characters long. Please select again.");
   var confirmCharLength = (prompt("How maany character would you like your password?"));
 }
+//! don't repeat yourself, abstract into a function that you can resuse.  
+//! Figure out way to add these and loop over the questions and save the answers.  
+
+//! Store messages somewhere separate.
+
+//! Long term explore using checkboxes (form inputs) instead of prompts.
 // Variable to store number characters
 var confirmCharNumb = confirm ("Would you like to use numbers?");
 // Variable to store special characters 
@@ -59,6 +71,7 @@ var confirmCharUpp = confirm ("Would you like to use uppercase letters?");
 
   // Function to get random array
 
+  //! define in a separate function.
   var RandomPasswordGen = ""
   for (var i = 0; i < confirmCharLength; i++) {
     RandomPasswordGen = RandomPasswordGen + passwordChar[Math.floor(Math.random() * passwordChar.length)];
